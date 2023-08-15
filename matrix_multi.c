373ac870e3502340a03402c96081832023bf7c6b
Aman Goel <124771341+Wolfie8935@users.noreply.github.com>
2023-08-15 23:14:55 +0530

    Update matrix_multi.c
    corrected the code

--- matrix_multi.c
+++ matrix_multi.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h> // Include this header for malloc and calloc
 
-int main(){
+int main() {
 
     int mat1[3][3], mat2[3][3];
 
     printf("Enter matrix 1: \n");
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            scanf("%d",&mat1[i][j]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &mat1[i][j]);
         }
-    } 
+    }
 
     printf("Enter matrix 2: \n");
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            scanf("%d",&mat2[3][3]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &mat2[i][j]); // Use mat2[i][j] instead of mat2[3][3]
         }
     }
 
-    int** mat3[3][3] = (int**) malloc (3*sizeof(int*));
-    for (int i=0; i<3; i++){
-        for (int  j=0; j<3; j++){
-            mat3[i][j] = (int*) calloc (3,sizeof(int));
-        }
+    int** mat3 = (int**)malloc(3 * sizeof(int*)); 
+        for (int i = 0; i < 3; i++) {
+        mat3[i] = (int*)calloc(3, sizeof(int));
     }
 
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            for (int k=0; k<3; k++){
-                mat3[i][j] = mat1[i][k] * mat2[k][j];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                mat3[i][j] += mat1[i][k] * mat2[k][j]; 
             }
         }
     }
-    printf("Matrix multipliction is: \n");
-    for (int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            printf("%d ",mat3[i][j]);
+    printf("Matrix multiplication is: \n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", mat3[i][j]);
         }
+        printf("\n"); 
     }
+
+    // Free allocated memory
+    for (int i = 0; i < 3; i++) {
+        free(mat3[i]);
+    }
+    free(mat3);
 
     return 0;
 }
